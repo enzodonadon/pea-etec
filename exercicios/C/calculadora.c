744864ba25269e15d6d0b9f1243717f00e5bd13c
enzodonadon <empdonadon@gmail.com>
2026-08-26 11:43:48 -0300

    add exercicio calculadora pea-etec

---
+++ exercicios/C/calculadora.c
@@ -0,0 +1,52 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include "conta.h"
+
+int main() {
+    char operacao;
+    double num1, num2, result;
+
+    printf("Escolha a operacao (+, -, *, /): ");
+    scanf("%c", &operacao);
+
+    printf("Digite o primeiro numero: ");
+    scanf("%lf", &num1);
+    
+    printf("Digite o segundo numero: ");
+    scanf("%lf", &num2);
+
+    switch(operacao) {
+        case '+':
+            result = soma(num1, num2);
+            printf("Resultado: %.2lf\n", result);
+            break;
+        case '-':
+            result = sub(num1, num2);
+            printf("Resultado: %.2lf\n", result);
+            break;
+        case '*':
+            result = mult(num1, num2);
+            printf("Resultado: %.2lf\n", result);
+            break;
+        case '/':
+            if(num2 != 0){
+                result = divisor(num1, num2);
+                printf("Resultado: %.2lf\n", result);
+            } else {
+                printf("Erro: Divisao por zero nao permitida.\n");
+            }
+            break;
+        default:
+            printf("Operacao invalida.\n");
+    }
+
+    return 0;
+}
+
---
+++ exercicios/C/conta.h
@@ -0,0 +1,21 @@
+#ifndef CONTA_H
+#define CONTA_H
+
+
+double soma(double num1, double num2){
+    return num1 + num2;
+}
+
+double sub(double num1, double num2){
+    return num1 - num2;
+}
+
+double mult(double num1, double num2){
+    return num1 * num2;
+}
+
+double divisor(double num1, double num2){
+    return num1 / num2;
+}
+
+#endif
